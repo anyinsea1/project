69ea81671ee09653b616aace5b4c417d9a7e6719
anyinsea1 <114900427+anyinsea1@users.noreply.github.com>
2025-03-18 19:31:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+<script>
+function showContent() {
+  let temp = document.getElementsByTagName("template")[0];
+  let clon = temp.content.cloneNode(true);
+  document.body.appendChild(clon);
+}
+</script>
+
+
+
+
+
+
+
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+
+
+    
+
